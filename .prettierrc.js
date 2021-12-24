7c9bd3895cff8c13f2941506e27fe761f28c7ae5
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2021-12-24 17:26:45 +0300

    disable semi column

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
-  semi: true,
+  semi: false,
   trailingComma: 'all',
   tabWidth: 2,
   endOfLine: "auto",
