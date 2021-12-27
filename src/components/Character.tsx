cd8a109d3fb22c4b93b0aee8e49e860efe707f94
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2021-12-27 17:51:21 +0300

    Character compoennt for flat list

---
+++ src/components/Character.tsx
@@ -0,0 +1,41 @@
+import React, { useCallback } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { screenHeight, screenWidth } from '../constants/sizes'
+import { ICharacter } from '../interfaces/ICharacter'
+
+interface Props {
+  item: ICharacter
+  onPress: (item: ICharacter) => void
+}
+const Character = (props: Props) => {
+  const { item, onPress } = props
+
+  const onPressCb = useCallback(() => {
+    onPress(item)
+  }, [item])
+
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      activeOpacity={0.7}
+      onPress={onPressCb}>
+      <View>
+        <Text>{item.name}</Text>
+        <Text>{item.species}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+export default Character
+
+const styles = StyleSheet.create({
+  container: {
+    width: screenWidth / 2.4,
+    margin: 4,
+    backgroundColor: '#EBEDF0',
+    padding: 4,
+    borderRadius: 4,
+    height: screenHeight / 10,
+  },
+})
