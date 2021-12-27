742d3a2bee2bbbfabeb67732cae0966e23b62cfa
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2021-12-27 19:12:58 +0300

    adding color palette

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -3,3 +3,26 @@ export const lightTextColor = 'rgba(255, 255, 255, 0.96)'
 export const backgroundColor = '#ffffff'
 
 export const borderColor = '#F0F0F0'
+
+export const palette = {
+    primary: {
+        dark: '#3568D4',
+        main: '#3E7BFA',
+        light: '#6698FA',
+        subtle: '#CCDDFF',
+    },
+    secondary: {
+        dark: '#FFCC00',
+        main: '#FDDD48',
+        light: '#FDED72',
+        subtle: '#FFFEE5',
+    },
+    text: {
+        primary: '#1C1C28',
+        secondary: '#28293D',
+        disabled: '#8F90A6',
+    },
+    background: '#F5F6F8',
+    divider: '#D7DCE1',
+    error: '#FF3B3B',
+}
